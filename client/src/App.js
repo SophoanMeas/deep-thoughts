43423c8085951a2f2f2abca08142966f298c6524
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-07 03:20:50 -0400

    feature/client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
-  ApolloProvider,
   ApolloClient,
   InMemoryCache,
+  ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
@@ -29,19 +30,34 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className='flex-column justify-flex-start min-100-vh'>
+        <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-          <div className='container'>
+          <div className="container">
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/signup' element={<Signup />} />
-              <Route path='/profile'>
-                <Route path=':username' element={<Profile />} />
-                <Route path='' element={<Profile />} />
-              </Route>
-              <Route path='/thought/:id' element={<SingleThought />} />
-              <Route path='*' element={<NoMatch />} />
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
             </Routes>
           </div>
           <Footer />
@@ -50,4 +66,5 @@ function App() {
     </ApolloProvider>
   );
 }
+
 export default App;
--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,48 +1,7 @@
 import React from 'react';
-import './404Style.css';
 
 const NoMatch = () => {
-
-  return (
-    <div class='box'>
-      <div class='box__ghost'>
-        <div class='symbol'></div>
-        <div class='symbol'></div>
-        <div class='symbol'></div>
-        <div class='symbol'></div>
-        <div class='symbol'></div>
-        <div class='symbol'></div>
-
-        <div class='box__ghost-container'>
-          <div class='box__ghost-eyes'>
-            <div class='box__eye-left'></div>
-            <div class='box__eye-right'></div>
-          </div>
-          <div class='box__ghost-bottom'>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </div>
-        <div class='box__ghost-shadow'></div>
-      </div>
-
-      <div class='box__description'>
-        <div class='box__description-container'>
-          <div class='box__description-title'>Whoops!</div>
-          <div class='box__description-text'>
-            It seems like we couldn't find the page you were looking for
-          </div>
-        </div>
-
-        <a href='/' target='_blank' class='box__button'>
-          Go back
-        </a>
-      </div>
-    </div>
-  );
+  return <div>Oops, we couldn't find that page.</div>;
 };
 
 export default NoMatch;
